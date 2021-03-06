e056fceaf915257b4f962b2161575164ab0082be
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-03-06 12:14:00 +0100

    fix admin dashboard and distinction with student

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,32 +20,6 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if(Auth::user()->role == 'admin')
-            <div class="box" style="width: 100%">
-                <header class="admin" style="margin: 0px !important">
-                    <div class="items">
-                        <a href="#user" class="item active"><i class="fas fa-home"></i> Home</a>
-                        <a href="#login" class="item"><i class="fas fa-user-lock"></i> Login</a>
-                        <a href="#test" class="item"><i class="fas fa-vial"></i> Test</a>
-                    </div>
-                    <p class="user"><i class="fas fa-user"></i> Admin</p>
-                </header>
-            </div>
-            @endif
-            @if(Auth::user()->role == 'alumne')
-            <div class="box" style="width: 100%">
-                <header style="margin: 0px !important">
-                    <div class="items">
-                        <a href="#user" class="item active"><i class="fas fa-home"></i> Home</a>
-                        <a href="#login" class="item"><i class="fas fa-user-lock"></i> Login</a>
-                        <a href="#test" class="item"><i class="fas fa-vial"></i> Test</a>
-                    </div>
-                    <p class="user"><i class="fas fa-user"></i> Student</p>
-                </header>
-            </div>
-            @endif
             <!-- Page Content -->
             <main>
                 {{ $slot }}
