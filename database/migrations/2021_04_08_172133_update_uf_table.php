3e78893bdd5a2c48efd7e01aef4fd0cd28208676
JesusSePe <umbreon20019@gmail.com>
2021-04-09 15:14:26 +0200

    CRUD UFs

---
+++ database/migrations/2021_04_08_172133_update_uf_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUfTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('uf', function (Blueprint $table){
+            $table->boolean('active')->default(1);
+        });
+        //
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('uf', function (Blueprint $table){
+            $table->dropColumn('active');
+        });
+    }
+}
