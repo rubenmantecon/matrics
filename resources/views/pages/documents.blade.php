0873d435dc446126d9105e275109fe0c9fd8a703
Pradas137 <pradas137@gmail.com>
2021-04-09 17:20:19 +0200

    form action

--- resources/views/pages/documents.blade.php
+++ resources/views/pages/documents.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout page="documents"> 
     <div>
+    <form action="/guardar" method="post" enctype="multipart/form-data" >
         <label for="DNI">Selecciona el DNI:</label>
         <input type="file" name="DNI" id="DNI" multiple><br>
         <label for="Tarjeta sanitaria">Selecciona el Tarjeta sanitaria:</label>
@@ -9,6 +10,7 @@
         <label for="Resguard del pagament">Selecciona el Resguard del pagament:</label>
         <input type="file" id="Resguard del pagament" multiple><br>
         <input class="btn primary-btn"type="submit" value="Pujar arxius">
+    </form>
     </div>
 </x-app-layout>
 
