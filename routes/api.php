0aa29d8f5af6006a52d7027bf36c21f3af6f5506
JesusSePe <umbreon20019@gmail.com>
2021-04-10 17:45:48 +0200

    Clone term API Updated Mp, Uf and Career models to support mass-assignment

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Term;
+use App\Models\Career;
+use App\Models\Mp;
+use App\Models\Uf;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +21,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// Duplicate a specific Term with every career, MP and UF associated.
+Route::post('/duplicate/{id}', function(Request $request) {
+    $data = ['status' => 'Unauthorized, error 503'];
+    $token = $request->header('token');
+    if ($token) {
+        $user = User::select("token")->where('token', $token)->get()[0];
+        if ($user['token']) {
+            $term = Term::find($request->route('id'));
+            $newTerm = $term->replicate();
+            $newTerm->save();
+            foreach (Career::where('term_id', $term->id)->cursor() as $career) {
+                $newCareer = $career->replicate()->fill(['term_id' => $newTerm->id]);
+                $newCareer->save();
+                foreach (Mp::where('career_id', $career->id)->cursor() as $mp){
+                    $newMp = $mp->replicate()->fill(['career_id' => $newCareer->id]);
+                    $newMp->save();
+                    foreach (Uf::where('mp_id', $mp->id)->cursor() as $uf) {
+                        $newUf = $uf->replicate()->fill(['mp_id' => $newMp->id]);
+                        $newUf->save();
+                    }
+                }
+            }
+            $data = ['status' => 'done'];
+        }
+    }
+    return response()->json($data);
 });
