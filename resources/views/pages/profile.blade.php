c162a1265abf01682a73ee3d3a68b1baaad38864
Pradas137 <pradas137@gmail.com>
2021-04-11 16:50:21 +0200

    firstname

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -14,7 +14,7 @@
                 
                 <div class="form-group">
                     <label for="state.cognom">COGNOM</label>
-                    <p>{{ $data->lastname }}</p>
+                    <p>{{ $data->firstname }}</p>
                 </div>
 
                 <div class="form-group">
