0f12e124beea9cdf1e094887bf18c123e782c82b
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-04-11 20:49:30 +0200

    adapt language

--- resources/views/pages/createAdmin.blade.php
+++ resources/views/pages/createAdmin.blade.php
@@ -13,7 +13,7 @@
 
             <!-- Username -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="username" :value="__('Username')" />
+                <x-label for="username" :value="__('Usuari')" />
 
                 <x-input id="username" class="input block mt-1 h-8" type="text" name="username" :value="old('username')" required
                     autofocus />
@@ -21,7 +21,7 @@
 
             <!-- Name -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="firstname" :value="__('Firstame')" />
+                <x-label for="firstname" :value="__('Nom')" />
 
                 <x-input id="firstname" class="input block mt-1 h-8" type="text" name="firstname" :value="old('firstname')" required
                     autofocus />
@@ -29,7 +29,7 @@
 
             <!-- Surname -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="lastname1" :value="__('Lastname1')" />
+                <x-label for="lastname1" :value="__('Primer cognom')" />
 
                 <x-input id="lastname1" class="input block mt-1 h-8" type="text" name="lastname1" :value="old('lastname1')" required
                     autofocus />
@@ -37,7 +37,7 @@
 
             <!-- Second Surname -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="lastname2" :value="__('Lastname2')" />
+                <x-label for="lastname2" :value="__('Segon cognom')" />
 
                 <x-input id="lastname2" class="input block mt-1 h-8" type="text" name="lastname2" :value="old('lastname2')" required
                     autofocus />
@@ -45,14 +45,14 @@
 
             <!-- Email Address -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('Correu electrònic')" />
 
                 <x-input id="email" class="input block mt-1 h-8" type="email" name="email" :value="old('email')" required />
             </div>
 
             <!-- Password -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Contrasenya')" />
 
                 <x-input id="password" class="input block mt-1 h-8" type="password" name="password" required
                     autocomplete="new-password" />
@@ -60,7 +60,7 @@
 
             <!-- Confirm Password -->
             <div class="label-group-admin grid sm:grid-cols-2">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-label for="password_confirmation" :value="__('Confirma contrasenya')" />
 
                 <x-input id="password_confirmation" class="input block mt-1 h-8" type="password" name="password_confirmation"
                     required />
@@ -68,11 +68,11 @@
 
             <div class="flex items-center justify-center mt-4">
                 <a class="underline text-sm text-yellow-600 hover:text-yellow-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Ja estàs registrat?') }}
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registra') }}
                 </x-button>
             </div>
         </form>
