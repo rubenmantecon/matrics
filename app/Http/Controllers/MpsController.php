4efa087b92334b28c33a105f22197c50afc247f1
byrambo16 <byrambo166@gmail.com>
2021-04-12 15:55:54 +0200

    Mps controller finished

--- app/Http/Controllers/MpsController.php
+++ app/Http/Controllers/MpsController.php
@@ -9,6 +9,10 @@ use App\Models\Career;
 
 use Illuminate\Http\Request;
 
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
 class MpsController extends Controller
 {
     /**
@@ -46,7 +50,22 @@ class MpsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = ['status' => 'Unauthorized, error 503'];
+        $token = $request->header('token');
+        $parsedReq = json_decode($request->data, true);
+        if ($token) {
+            $mp = new Mp;
+            $mp->career_id = $parsedReq['career_id'];
+            $mp->code = $parsedReq['code'];
+            $mp->name = $parsedReq['name'];
+            $mp->description = $parsedReq['description'];
+            $status = $mp->save();
+            if ($status) {
+                $data = ["status" => "Nou modul creat correctament."];
+                Log::channel('dblogging')->info("Ha creado un nuevo Modulo", ["user_id" => Auth::id(), "mp_id" => $mp->id]);
+            }
+        }
+        return response()->json($data);
     }
 
     /**
@@ -80,7 +99,34 @@ class MpsController extends Controller
      */
     public function update(Request $request, Mp $mp)
     {
-        //
+        $parsedReq = json_decode($request->data, true);
+        $data = ['status' => 'Unauthorized, error 503'];
+        $token = $request->header('token');
+        if ($token) {
+            $user = User::select("token")->where('token', $token)->where("role", "admin")->get()[0];
+            if ($user['token']) {
+                if ($request->type === "softDelete") {
+                    $mp->active = 0;
+                } else {
+                    $mp->name = $parsedReq->name;
+                    $mp->description = $parsedReq['description'];
+                    $mp->code = $parsedReq['code'];
+                }
+                $mp->touch();
+
+                $status = $mp->save();
+                if ($status)
+                    $data = ["status" => "MOdulo actualitzat correctament."];
+
+                if ($request->type === "softDelete") {
+                    $data = ["status" => "Modul eliminat correctament."];
+                    Log::channel('dblogging')->info("Ha eliminado un Modulo", ["user_id" => Auth::id(), "mp_id" => $mp->id]);
+                } else {
+                    $data = ["status" => "Modul actualitzat correctament."];
+                    Log::channel('dblogging')->info("Ha actualizado un Modulo", ["user_id" => Auth::id(), "mp_id" => $mp->id]);
+                }
+            }
+        }
     }
 
     /**
@@ -91,6 +137,5 @@ class MpsController extends Controller
      */
     public function destroy(Mp $mp)
     {
-        //
     }
 }
