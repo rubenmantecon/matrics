a642306bc3353830119cdafe32fa95c83ff6862f
Pradas137 <pradas137@gmail.com>
2021-04-16 15:45:04 +0200

    nuevo

---
+++ database/migrations/2021_04_15_175220_update_enrolments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateEnrolmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('enrolments', function (Blueprint $table) {
+            $table->date('birth_date');
+            $table->string('birth_place');
+            $table->string('address');
+            $table->string('population');
+            $table->integer('postal_code', 9);
+            $table->integer('phone_number', 15);
+            $table->integer('emergency_number', 15);
+            $table->string('father_name');
+            $table->string('father_dni');
+            $table->string('mother_name');
+            $table->string('mother_dni');
+        });
+        //
+
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
