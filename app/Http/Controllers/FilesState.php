9754ae0818bd93c7c4928f58a4750c72a7ab1889
kokodemo <xnominatorxx@gmail.com>
2021-04-19 16:15:03 +0200

    migration req_enrol

--- app/Http/Controllers/FilesState.php
+++ app/Http/Controllers/FilesState.php
@@ -8,12 +8,9 @@ class FilesState extends Controller
 {
     //
     public function __invoke() {
-    	$actual=Auth::user()->id ;
-    	$dni= DB::select('SELECT rq.state FROM req_enrol rq, requirements rs, enrolments e WHERE e.user_id = ? and profile_id = 1 and e.id = rq.enrolment_id and rs.req_id = rq.id', [$actual]);
-    	$data = array();
-    	array_push($data, $dni) ;
-    	$sanitaria= DB::select('SELECT rq.state FROM req_enrol, requirements rs, enrolments e WHERE e.user_id = ? and profile_id = 2 and e.id = rq.enrolment_id and rs.req_id = rq.id', [$actual]);
-    	$title= DB::select('SELECT rq.state FROM req_enrol, requirements rs, enrolments e WHERE e.user_id = ? and profile_id = 3 and e.id = rq.enrolment_id and rs.req_id = rq.id', [$actual]);   
-    	return response()->json($data);
-    }
+
+    $user= Auth::user();
+	$req_enrols=$user->enrolments().first->req_enrols()
+	return render("semafor",["req_enrols"=>$req_enrols]);
+}
 }
