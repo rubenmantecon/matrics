40cf6718ed4a5d93d4def1df83ad35d36ca60650
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2021-04-19 18:43:08 +0200

    fix: reapply CSS & breadcrumbs from cherrypick

--- resources/views/pages/requirements.blade.php
+++ resources/views/pages/requirements.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout page="requirements">
-    <div class="center">
 	@section('breadcrumbs')
         {{ Breadcrumbs::render('requirements') }}
     @endsection
@@ -15,28 +14,31 @@
 					</div>
 				@endforeach
 			@endif
-			<div>
-				<p>DERECHOS DE IMAGEN</p>
-				<input type="radio" id="pr_image_permit" name="pr_image" value="yes" required>
-				<label for="pr_image_permit"> Permetre</label>
-				<input type="radio" id="pr_image_deny" name="pr_image" value="no">
-				<label for="pr_image_deny"> Denegar</label>
-			</div>
-
-			<div>
-				<p>AUTORIZACION DE SALIDAS</p>
-				<input type="radio" id="pr_excursions_permit" name="pr_excursions" value="yes" required>
-				<label for="pr_excursions_permit"> Permetre</label>
-				<input type="radio" id="pr_excursions_deny" name="pr_excursions" value="no">
-				<label for="pr_excursions_deny"> Denegar</label>
-			</div>
-
-			<div>
-				<p>AUTORIZACION DE EXTRAESCOLARES</p>
-				<input type="radio" id="pr_extracurricular_permit" name="pr_extracurricular" value="yes" required>
-				<label for="pr_extracurricular_permit"> Permetre</label>
-				<input type="radio" id="pr_extracurricular_deny" name="pr_extracurricular" value="no">
-				<label for="pr_extracurricular_deny"> Denegar</label>
+			<div class="grid w-full justify-center ">
+				<div class="p-4">
+					<p>DERECHOS DE IMAGEN</p>
+					<input type="radio" id="pr_image_permit" name="pr_image" value="yes" required>
+					<label for="pr_image_permit"> Permetre</label>
+					<input type="radio" id="pr_image_deny" name="pr_image" value="no">
+					<label for="pr_image_deny"> Denegar</label>
+				</div>
+				
+				<div  class="p-4">
+					<p>AUTORIZACION DE SALIDAS</p>
+					<input type="radio" id="pr_excursions_permit" name="pr_excursions" value="yes" required>
+					<label for="pr_excursions_permit"> Permetre</label>
+					<input type="radio" id="pr_excursions_deny" name="pr_excursions" value="no">
+					<label for="pr_excursions_deny"> Denegar</label>
+				</div>
+				
+				<div class="p-4">
+					<p>AUTORIZACION DE EXTRAESCOLARES</p>
+					<input type="radio" id="pr_extracurricular_permit" name="pr_extracurricular" value="yes" required>
+					<label for="pr_extracurricular_permit"> Permetre</label>
+					<input type="radio" id="pr_extracurricular_deny" name="pr_extracurricular" value="no">
+					<label for="pr_extracurricular_deny"> Denegar</label>
+				</div>
+				<input type="submit" class="btn save my-4" value="Siguiente">
 			</div>
 
 			<!--
